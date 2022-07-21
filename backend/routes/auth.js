5fd311303afdffa6d97e3ccbe337b5b568350c00
NamitKumar16 <kumarnamit58@gmail.com>
2022-07-21 15:32:05 +0530

    Storing User details

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  obj = {
-    Hello: "world",
-  };
-  res.json(obj);
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send("Hello World!");
 });
 module.exports = router;
