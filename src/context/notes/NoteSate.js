921552537a90bf2090208cc2dfae917abe63a372
NamitKumar16 <kumarnamit58@gmail.com>
2022-08-25 22:42:05 +0530

    useLocation hook

--- src/context/notes/NoteSate.js
+++ src/context/notes/NoteSate.js
@@ -3,24 +3,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "N",
-    class: "N",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "A",
-        class: "A",
-      });
-    }, 1000);
-  };
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
-
 export default NoteState;
